cecc9a2b4314a192025c85c10092df6af92c87ce
Dylan Guzman <dylanaguzman@gmail.com>
2022-06-12 09:33:55 -0700

    update

---
+++ src/components/IconElement/IconElement.js
@@ -0,0 +1,13 @@
+import './IconElement.css'
+
+export const IconElement = ({icon, description, quote}) => {
+    return (
+        <div className={"icon-container"}>
+            <i className={icon}/>
+            <p className={"icon-element-text"}>{description}</p>
+            <br className={"break-point"}/>
+            <div className={"break-point"}/>
+            <p className={"element-quote"}>{quote}</p>
+        </div>
+    )
+}
--- src/screens/home-screen/HomeScreen.js
+++ src/screens/home-screen/HomeScreen.js
@@ -1,11 +1,14 @@
 import './HomeScreen.css';
 import Navbar from '../../components/Navbar/Navbar'
 import {Button} from '../../components/Buttons'
+import {IconElement} from "../../components/IconElement/IconElement";
 import {ImageElements} from "../../components/ImageElements/ImageElements";
+import {FaSearch} from "react-icons/fa"
 import ResidentialImage from '../../assets/RESIDENTIAL-IMAGE.png'
 import CommercialImage from '../../assets/COMMERCIAL-IMAGE.png'
 import EcoFriendlyImage from '../../assets/ECO-FRIENDLY-IMAGE.png'
 import SolutionsImage from '../../assets/SOLUTIONS-IMAGE.png'
+
 
 function HomeScreen() {
     return (
@@ -28,7 +31,7 @@ function HomeScreen() {
                 </div>
             </div>
             <div className="offerings-container">
-                <div className="offerings-header">
+                <div className="general-header">
                     <h2>Offerings</h2>
                 </div>
                 <div className="offerings-elements">
@@ -37,7 +40,16 @@ function HomeScreen() {
                     <ImageElements image={EcoFriendlyImage} description={"Eco-Friendly"}/>
                     <ImageElements image={SolutionsImage} description={"Solutions"}/>
                 </div>
-
+            </div>
+            <div className="rodent-control-container">
+                <div className="general-header">
+                    <h2>How rodent control works</h2>
+                </div>
+                <div className="rodent-control-elements">
+                    <IconElement icon={"fa-solid fa-magnifying-glass"} description={"Rodent inspection"} quote={"Your Impact Pest Pros professional will conduct a thorough inspection to determine where and how rodents are entering your home. "}/>
+                    <IconElement icon={"fa-solid fa-user-shield"} description={"Exclusion"} quote={"First, we will remove any rodents from your home. Then, we will seal up entry points to keep them from re-entering your home. "}/>
+                    <IconElement icon={"fa-solid fa-house-chimney"} description={"Annual inspection"} quote={"We will conduct an annual inspection each year for as long as you keep your plan. We will inspect previous entry point, as well as new vulnerabilities "}/>
+                </div>
             </div>
         </div>
     );
